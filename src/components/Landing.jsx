e7ed299664bfd1e357335c6ba3746f90f1f4b985
Austin <austinematurure@gmail.com>
2024-12-12 08:52:52 +0200

    feat: added navbar and landing page

---
+++ src/components/Landing.jsx
@@ -0,0 +1,27 @@
+import "../css/landing.css";
+import landingImg from "../assets/landing-img.jpg";
+import arrow from "../assets/arrow-down-circle-svgrepo-com.svg";
+
+export default function Landing() {
+  return (
+    <>
+      <section
+        className="landing-cnt"
+        style={{
+          backgroundImage: `linear-gradient(to left, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0)), url(${landingImg})`,
+        }}
+      >
+        <div className="landing">
+          <div className="landing-text">
+            <h1>BOOK YOUR COURT NOW</h1>{" "}
+            <p>@ THE ONLY PADEL COURT IN PIET RETIEF</p>{" "}
+            <div className="learn-more">
+              <button>LEARN MORE</button>
+              <img src={arrow} alt="" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import "../css/navbar.css";
+
+export default function Navbar() {
+  return (
+    <>
+      <header className="navbar-header">
+        <nav className="navbar">
+          <div className="navbar-logo">
+            <a href="/">
+              <h1>UlTIMATE PADEL PIET RETIEF</h1>
+            </a>
+            <p>@ Family & Fun</p>
+          </div>
+          <div className="nav-items">
+            <ul className="nav-menu">
+              <li>
+                <a href="#home">Home</a>
+              </li>
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <a href="#services">Services</a>
+              </li>
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+
+            <div className="navbar-cta">
+              <a href="#call" className="btn btn-call">
+                068 000 0333
+              </a>
+            </div>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+}
